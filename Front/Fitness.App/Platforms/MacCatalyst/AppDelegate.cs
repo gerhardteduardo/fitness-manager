88f9b82750c6e5f67ebb075cfab7f13f112bfb61
Eduardo Gerhardt <comercial.eduardogerhardt@gmail.com>
2025-09-18 00:29:39 -0300

    fix: solution and project name

---
+++ Front/Fitness.App/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Fitness.App
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
