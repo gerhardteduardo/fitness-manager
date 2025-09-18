25226d6d2a86119d8d9974c6f1073751055e85da
Eduardo Gerhardt <comercial.eduardogerhardt@gmail.com>
2025-09-18 08:30:24 -0300

    feat: add first model

--- Front/Fitness.Core/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Fitness.Core
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ Front/Fitness.Core/Models/Client.cs
@@ -0,0 +1,10 @@
+﻿namespace Fitness.Core.Models
+{
+    public class Client
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public double Weight { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
